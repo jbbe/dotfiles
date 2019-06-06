936c1928d207ae87ce750ac50db5e1f13ef9fdba
jbbe <jbbe@umich.edu>
2019-06-06 12:43:28 -0400

    Create flatpak.sh

---
+++ flatpak.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+apt update && apt upgrade
+apt install flatpak
+apt install gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.gnome.Builder
+flatpak install flathub org.freedesktop.Bustle
